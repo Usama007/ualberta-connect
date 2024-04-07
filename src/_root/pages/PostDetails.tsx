4158c3166c7c2482ad736cdb9a9913477ace8973
Usama007 <usamacsse53@gmail.com>
2024-04-07 06:32:20 +0600

    edit post

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -110,23 +110,24 @@ const PostDetails = () => {
                   />
                 </Link>
 
-                {isDeleteLoading ? (
-                  <Loader />
-                ) : (
-                  <Button
-                    onClick={handleDeletePost}
-                    variant="ghost"
-                    className={`ost_details-delete_btn ${
-                      user.id !== post?.creator.$id && "hidden"
-                    }`}>
+                <Button
+                  onClick={handleDeletePost}
+                  disabled={!isDeleteLoading}
+                  variant="ghost"
+                  className={`ost_details-delete_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}>
+                  {!isDeleteLoading ? (
+                    <Loader />
+                  ) : (
                     <img
                       src={"/assets/icons/delete.svg"}
                       alt="delete"
                       width={24}
                       height={24}
                     />
-                  </Button>
-                )}
+                  )}
+                </Button>
               </div>
             </div>
 
